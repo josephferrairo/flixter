bd8ded589e148406b31c7e1102a9fc4ede363c5e
josephferrairo <josephferrairo@gmail.com>
2015-08-31 03:06:00 +0000

    added lessons

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Flixter::Application.routes.draw do
   root 'static_pages#index'
   resources :courses, :only =>[:index, :show]
   namespace :instructor do
+    resources :sections, :only =>[] do
+      resources :lessons, :only => [:new, :create]
+    end
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:new, :create]
-    
+      end
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
@@ -62,4 +65,3 @@ Flixter::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
